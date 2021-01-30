7e296666e2bbbefc62fdcc60a25f551269a7c3e2
Harnet69 <harnet69@gmail.com>
2021-01-30 14:09:45 +0100

    Implemented swipecard xml

--- app/src/main/java/com/harnet/codecommunity/view/TechsChooserFragment.kt
+++ app/src/main/java/com/harnet/codecommunity/view/TechsChooserFragment.kt
@@ -10,6 +10,7 @@ import androidx.databinding.DataBindingUtil
 import com.harnet.codecommunity.R
 import com.harnet.codecommunity.databinding.TechsChooserFragmentBinding
 import com.harnet.codecommunity.viewModel.TechsChooserViewModel
+import com.lorentzos.flingswipe.SwipeFlingAdapterView
 
 class TechsChooserFragment : Fragment() {
     private lateinit var dataBinding: TechsChooserFragmentBinding
@@ -19,7 +20,8 @@ class TechsChooserFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        dataBinding = DataBindingUtil.inflate(inflater, R.layout.techs_chooser_fragment, container, false)
+        dataBinding =
+            DataBindingUtil.inflate(inflater, R.layout.techs_chooser_fragment, container, false)
         viewModel = ViewModelProvider(this).get(TechsChooserViewModel::class.java)
 
         return dataBinding.root
@@ -28,6 +30,59 @@ class TechsChooserFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        dataBinding.textTest.text = "I'm binded!"
+        //add the view via xml or programmatically
+        val flingContainer = dataBinding.swipeCardsFrame
+
+
+//        al = new ArrayList < String >();
+//        al.add("php");
+//        al.add("c");
+//        al.add("python");
+//        al.add("java");
+//
+//        //choose your favorite adapter
+//        arrayAdapter = new ArrayAdapter < String >(this, R.layout.item, R.id.helloText, al);
+//
+//        //set the listener and the adapter
+//        flingContainer.setAdapter(arrayAdapter);
+//        flingContainer.setFlingListener(new SwipeFlingAdapterView . onFlingListener () {
+//            @Override
+//            public void removeFirstObjectInAdapter() {
+//                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+//                Log.d("LIST", "removed object!");
+//                al.remove(0);
+//                arrayAdapter.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onLeftCardExit(Object dataObject) {
+//                //Do something on the left!
+//                //You also have access to the original object.
+//                //If you want to use it just cast it (String) dataObject
+//                Toast.makeText(MyActivity.this, "Left!", Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onRightCardExit(Object dataObject) {
+//                Toast.makeText(MyActivity.this, "Right!", Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onAdapterAboutToEmpty(int itemsInAdapter) {
+//                // Ask for more data here
+//                al.add("XML ".concat(String.valueOf(i)));
+//                arrayAdapter.notifyDataSetChanged();
+//                Log.d("LIST", "notified");
+//                i++;
+//            }
+//        });
+//
+//        // Optionally add an OnItemClickListener
+//        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView . OnItemClickListener () {
+//            @Override
+//            public void onItemClicked(int itemPosition, Object dataObject) {
+//                makeToast(MyActivity.this, "Clicked!");
+//            }
+//        });
     }
 }
