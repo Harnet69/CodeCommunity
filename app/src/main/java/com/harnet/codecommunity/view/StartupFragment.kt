99dde8efbdd77a932fac68db8cc0b1247ca056c5
Harnet69 <harnet69@gmail.com>
2021-01-31 17:23:21 +0100

    Implemented startup fragment

---
+++ app/src/main/java/com/harnet/codecommunity/view/StartupFragment.kt
@@ -0,0 +1,28 @@
+package com.harnet.codecommunity.view
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.harnet.codecommunity.R
+import com.harnet.codecommunity.viewModel.StartupViewModel
+
+class StartupFragment : Fragment() {
+
+    private lateinit var viewModel: StartupViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewModel = ViewModelProvider(this).get(StartupViewModel::class.java)
+        return inflater.inflate(R.layout.startup_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/harnet/codecommunity/viewModel/StartupViewModel.kt
@@ -0,0 +1,7 @@
+package com.harnet.codecommunity.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class StartupViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
