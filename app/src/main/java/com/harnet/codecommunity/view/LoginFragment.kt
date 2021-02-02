7bdd02e380fa309aa7a191687198bce5c2e32590
Harnet69 <harnet69@gmail.com>
2021-02-02 10:20:18 +0100

    Implemented signUp xml

--- app/src/main/java/com/harnet/codecommunity/view/LoginFragment.kt
+++ app/src/main/java/com/harnet/codecommunity/view/LoginFragment.kt
@@ -6,28 +6,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.harnet.codecommunity.viewModel.LoginViewModel
 import com.harnet.codecommunity.R
+import com.harnet.codecommunity.databinding.LoginFragmentBinding
 
 class LoginFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = LoginFragment()
-    }
-
     private lateinit var viewModel: LoginViewModel
+    private lateinit var dataBinding: LoginFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.login_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    ): View {
         viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
-        // TODO: Use the ViewModel
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.login_fragment, container, false)
+
+        return dataBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
--- app/src/main/java/com/harnet/codecommunity/view/SignUpFragment.kt
+++ app/src/main/java/com/harnet/codecommunity/view/SignUpFragment.kt
@@ -6,28 +6,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.harnet.codecommunity.R
+import com.harnet.codecommunity.databinding.SignUpFragmentBinding
 import com.harnet.codecommunity.viewModel.SignUpViewModel
 
 class SignUpFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = SignUpFragment()
-    }
-
     private lateinit var viewModel: SignUpViewModel
+    private lateinit var dataBinding: SignUpFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.sign_up_fragment, container, false)
+    ): View {
+        viewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.sign_up_fragment, container, false)
+
+        return dataBinding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
 
     }
-
 }
