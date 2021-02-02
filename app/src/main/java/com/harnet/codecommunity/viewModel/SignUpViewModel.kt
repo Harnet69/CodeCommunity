4825436268ace6e606a68f6132866a5bc398de54
Harnet69 <harnet69@gmail.com>
2021-02-02 16:58:28 +0100

    Fixed Firebase dependencies issues

--- app/src/main/java/com/harnet/codecommunity/viewModel/SignUpViewModel.kt
+++ app/src/main/java/com/harnet/codecommunity/viewModel/SignUpViewModel.kt
@@ -1,7 +1,13 @@
 package com.harnet.codecommunity.viewModel
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpViewModel : ViewModel() {
+    private var firebaseAuth = FirebaseAuth.getInstance()
+    // listens for a state firebase authentication. Called when user created
+    private var firebaseAuthListener = FirebaseAuth.AuthStateListener {
+
+    }
 
 }
