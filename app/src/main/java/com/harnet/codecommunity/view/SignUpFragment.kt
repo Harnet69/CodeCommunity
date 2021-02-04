064900466d63b9da8f1430be2c780df98543a03d
Harnet69 <harnet69@gmail.com>
2021-02-04 12:36:40 +0100

    Implemented a bilerplate of firebase signUp

--- app/src/main/java/com/harnet/codecommunity/view/SignUpFragment.kt
+++ app/src/main/java/com/harnet/codecommunity/view/SignUpFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import com.harnet.codecommunity.R
 import com.harnet.codecommunity.databinding.SignUpFragmentBinding
 import com.harnet.codecommunity.di.DaggerFormValidatorHelperComponent
@@ -39,12 +41,24 @@ class SignUpFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         onSignUp()
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel(){
+        viewModel.mIsUserCreated.observe(viewLifecycleOwner, {
+            if(it)
+                Navigation.findNavController(dataBinding.userEmailSignUp).navigate(SignUpFragmentDirections.actionSignUpFragmentToTechsChooserFragment())
+        })
     }
 
     private fun onSignUp() {
         dataBinding.signUpBtnSignUp.setOnClickListener {
             if (isFormValid()) {
                 Toast.makeText(context, "register User", Toast.LENGTH_SHORT).show()
+                // create new user
+                viewModel.createUser(dataBinding.userEmailSignUp.editText?.text.toString(),
+                                    dataBinding.userPasswordSignUp.editText?.text.toString())
             }
         }
     }
--- app/src/main/java/com/harnet/codecommunity/viewModel/SignUpViewModel.kt
+++ app/src/main/java/com/harnet/codecommunity/viewModel/SignUpViewModel.kt
@@ -1,14 +1,15 @@
 package com.harnet.codecommunity.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 
 class SignUpViewModel : ViewModel() {
     private var firebaseAuth = FirebaseAuth.getInstance()
     // listens for a state firebase authentication. Called when user created
-    private var firebaseAuthListener = FirebaseAuth.AuthStateListener {
+    private var firebaseAuthListener = FirebaseAuth.AuthStateListener {}
 
-    }
+    val mIsUserCreated = MutableLiveData<Boolean>()
 
     //add firebase
     fun addFirebaseListener(){
@@ -18,6 +19,15 @@ class SignUpViewModel : ViewModel() {
     //remove firebase
     fun removeFirebaseListener(){
         firebaseAuth.removeAuthStateListener(firebaseAuthListener)
+    }
+
+    // create new user
+    fun createUser(userEmail: String, userPsw: String){
+//        firebaseAuth.createUserWithEmailAndPassword(userEmail, userPsw)
+//            .addOnCompleteListener {
+//
+//            }
+        mIsUserCreated.value = true
     }
 
 }
