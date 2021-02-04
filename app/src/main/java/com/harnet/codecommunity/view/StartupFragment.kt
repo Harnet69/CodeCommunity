fd93fd201443dd4c0e83550b76a129ecf03a22df
Harnet69 <harnet69@gmail.com>
2021-02-04 14:17:52 +0100

    Implemented logging check

--- app/src/main/java/com/harnet/codecommunity/view/StartupFragment.kt
+++ app/src/main/java/com/harnet/codecommunity/view/StartupFragment.kt
@@ -2,12 +2,17 @@ package com.harnet.codecommunity.view
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
 import com.harnet.codecommunity.R
 import com.harnet.codecommunity.databinding.StartupFragmentBinding
 
@@ -25,6 +30,12 @@ class StartupFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //check if user have been logged already
+        if(FirebaseAuth.getInstance().currentUser != null){
+            //TODO create a redirection if user is logged in
+            Toast.makeText(context, FirebaseAuth.getInstance().currentUser?.email, Toast.LENGTH_LONG).show()
+        }
+
         onLogin()
         onSignUp()
     }
