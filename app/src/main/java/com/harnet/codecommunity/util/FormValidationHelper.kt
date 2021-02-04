7a982d5a57ffe5a48c67653925d64cf14143bc33
Harnet69 <harnet69@gmail.com>
2021-02-04 11:49:36 +0100

    Refactored form validator helper

--- app/src/main/java/com/harnet/codecommunity/util/FormValidationHelper.kt
+++ app/src/main/java/com/harnet/codecommunity/util/FormValidationHelper.kt
@@ -8,9 +8,9 @@ class FormValidationHelper {
 
     //check is a field not empty
     fun fieldIsEmpty(textField: TextInputLayout): Boolean {
-        val required = "Required"
+        val errorMsg = "Required"
         if (TextUtils.isEmpty(textField.editText?.text.toString())) {
-            textField.error = required
+            textField.error = errorMsg
             return true
         }
         textField.error = null
@@ -20,29 +20,32 @@ class FormValidationHelper {
 
     // check if an email format is valid
     fun isValidEmail(textField: TextInputLayout): Boolean {
+        val errorMsg = "Format is not valid"
         if (Patterns.EMAIL_ADDRESS.matcher(textField.editText?.text.toString()).matches()) {
             textField.error = null
             return true
         }
-        textField.error = "Format is not valid"
+        textField.error = errorMsg
 
         return false
     }
 
     // check if an username is valid
     fun isUsernameLengthValid(textField: TextInputLayout): Boolean {
+        val errorMsg = "3<=username length<=10"
         val usernameLength = textField.editText?.text.toString().length
         if (usernameLength in 10 downTo 3) {
             textField.error = null
             return true
         }
-        textField.error = "3<=username length<=10"
+        textField.error = errorMsg
 
         return false
     }
 
     // check if a password is strong enough
     fun isPasswordStrong(pswTextField: TextInputLayout): Boolean {
+        val errorMsg = "Password length >= 8 & contains: one digit, special char, upper case"
         val pswPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])(?=\\S+$).{4,}$"
         val pswMatcher = Regex(pswPattern)
         val psw = pswTextField.editText?.text.toString()
@@ -51,7 +54,7 @@ class FormValidationHelper {
             pswTextField.error = null
             return true
         }
-        pswTextField.error = "Password length >= 8 & contains: one digit, special char, upper case"
+        pswTextField.error = errorMsg
 
         return false
     }
