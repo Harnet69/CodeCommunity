47cd4950ad5aa41c5cd60f0ea21be8af8dd0175a
Harnet69 <harnet69@gmail.com>
2021-02-08 12:57:25 +0100

    Code refactor

--- app/src/main/java/com/harnet/codecommunity/util/FirebaseHelper.kt
+++ app/src/main/java/com/harnet/codecommunity/util/FirebaseHelper.kt
@@ -1,43 +1,28 @@
 package com.harnet.codecommunity.util
 
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.MutableLiveData
+import androidx.navigation.Navigation
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.harnet.codecommunity.R
 import com.harnet.codecommunity.model.User
+import com.harnet.codecommunity.view.MainActivity
+import com.harnet.codecommunity.view.StartupFragmentDirections
 
 class FirebaseHelper {
     private val USERS = "users"
-
     private var database = Firebase.database.reference
-
     private var firebaseAuth = FirebaseAuth.getInstance()
 
-    // listens for a state firebase authentication. Called when user created
-    private var firebaseAuthListener = FirebaseAuth.AuthStateListener {}
-
-    //add firebase listener
-    fun addFirebaseListener() {
-        firebaseAuth.addAuthStateListener(firebaseAuthListener)
-    }
-
-    //remove firebase listener
-    fun removeFirebaseListener() {
-        firebaseAuth.removeAuthStateListener(firebaseAuthListener)
-    }
-
-    fun isUserLogged(): Boolean {
-        if (FirebaseAuth.getInstance().currentUser != null) {
-            return true
-        }
-        return false
-    }
-
     // create new user
-    fun sighUp(mIsUserCreated: MutableLiveData<Boolean>, mUserCreatedFailureMsg: MutableLiveData<String>,
-               userEmail: String, userPsw: String) {
+    fun sighUp(
+        mIsUserCreated: MutableLiveData<Boolean>, mUserCreatedFailureMsg: MutableLiveData<String>,
+        userEmail: String, userPsw: String
+    ) {
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(userEmail, userPsw)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
@@ -52,29 +37,32 @@ class FirebaseHelper {
     private fun addUserToUsersDb() {
         val currentUser = FirebaseAuth.getInstance().currentUser
         val user = User(currentUser?.uid, "", "", currentUser?.email, "")
-        user.uid?.let { database.child(USERS).child(it).setValue(user)
-            .addOnSuccessListener {
-                Log.i("currentUser", "addUserToUsersDb: Success")
-            }
-            .addOnFailureListener { e ->
-                Log.i("currentUser", "Error! " + e.message)
-            }
+        user.uid?.let {
+            database.child(USERS).child(it).setValue(user)
+                .addOnSuccessListener {
+                    Log.i("currentUser", "addUserToUsersDb: Success")
+                }
+                .addOnFailureListener { e ->
+                    Log.i("currentUser", "Error! " + e.message)
+                }
         }
     }
 
-    fun logIn(mIsUserLoggedIn: MutableLiveData<Boolean>, mUserLoggingFailureMsg: MutableLiveData<String>,
-              userEmail: String, userPsw: String){
+    fun logIn(
+        mIsUserLoggedIn: MutableLiveData<Boolean>, mUserLoggingFailureMsg: MutableLiveData<String>,
+        userEmail: String, userPsw: String
+    ) {
         FirebaseAuth.getInstance().signInWithEmailAndPassword(userEmail, userPsw)
-            .addOnCompleteListener {task ->
-                if(task.isSuccessful){
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
                     mIsUserLoggedIn.value = true
-                }else{
+                } else {
                     mUserLoggingFailureMsg.value = task.exception?.localizedMessage.toString()
                 }
             }
     }
 
-    fun logOut(mIsUserLogged: MutableLiveData<Boolean>){
+    fun logOut(mIsUserLogged: MutableLiveData<Boolean>) {
         firebaseAuth.signOut()
         mIsUserLogged.value = false
     }
