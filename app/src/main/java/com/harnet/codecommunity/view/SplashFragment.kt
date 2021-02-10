a6c686031afd43df555e3734a08c5605ec8d1cb2
Harnet69 <harnet69@gmail.com>
2021-02-10 09:52:04 +0100

    Splash screen shows for 2 sec

--- app/src/main/java/com/harnet/codecommunity/view/SplashFragment.kt
+++ app/src/main/java/com/harnet/codecommunity/view/SplashFragment.kt
@@ -1,10 +1,15 @@
 package com.harnet.codecommunity.view
 
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.solver.widgets.Helper
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.harnet.codecommunity.R
 
 class SplashFragment : Fragment() {
@@ -13,5 +18,17 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.splash_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        Log.i("dfgdfg", "onViewCreated: Start")
+        Handler().postDelayed({
+            Navigation.findNavController(view)
+                .navigate(SplashFragmentDirections.actionSplashFragment3ToStartupFragment())
+        }, 2000)
+        Log.i("dfgdfg", "onViewCreated: Done")
     }
 }
